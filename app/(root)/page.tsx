13a94c96ff34e333988ac91e99eb44c80eb7b75f
will1467 <williamawrigley@gmail.com>
2025-02-10 12:39:42 +1100

    fix rating type issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import { getLatestProducts } from "@/lib/actions/product.actions";
 import ProductList from "@/components/shared/product/product-list";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
+import { Product } from "../types";
 const Home = async () => {
-  const latestProducts = await getLatestProducts();
+  const latestProducts: Product[] = await getLatestProducts();
   return <ProductList data={latestProducts} title='Newest Arrivals' limit={LATEST_PRODUCTS_LIMIT}/>
 }
  
