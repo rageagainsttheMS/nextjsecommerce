c934b07b8108b5de34708472204fa2a2d7a20d92
will1467 <williamawrigley@gmail.com>
2025-05-30 16:05:16 +1000

    stripe attempted fix

--- app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -3,13 +3,14 @@ import { getOrderByID } from "@/lib/actions/order.actions";
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-
 const SuccessPage = async (props: {
   params: Promise<{ id: string }>;
   searchParams: Promise<{ payment_intent: string }>;
 }) => {
+
+ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+
   const { id } = await props.params;
   const { payment_intent: intentId } = await props.searchParams;
 
